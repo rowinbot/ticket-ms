5cad09cbc6d545a9fb585a29b5a6057e8826a917
Rowin Hernandez <rowinkaleb@gmail.com>
2024-12-31 22:55:47 +0100

    fix: proxying to api

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_APP_API_URL as string,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [tsconfigPaths(), react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_APP_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
